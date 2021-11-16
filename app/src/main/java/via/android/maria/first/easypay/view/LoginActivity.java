a294d9ce0e7b58b4d51813ef1d0980396b94d42a
Nina8507 <cntp@outlook.dk>
2021-11-16 22:44:39 +0100

    login activity_xml created

---
+++ app/src/main/java/via/android/maria/first/easypay/view/LoginActivity.java
@@ -0,0 +1,11 @@
+package via.android.maria.first.easypay.view;
+
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    public void login(View view){
+
+    }
+}
