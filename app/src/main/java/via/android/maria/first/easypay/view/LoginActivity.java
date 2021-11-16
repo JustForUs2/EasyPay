ded20bab54e9faa0d2e4f49de288fcc810af3722
Nina8507 <cntp@outlook.dk>
2021-11-16 22:57:54 +0100

    started implementation login functionality

--- app/src/main/java/via/android/maria/first/easypay/view/LoginActivity.java
+++ app/src/main/java/via/android/maria/first/easypay/view/LoginActivity.java
@@ -1,10 +1,17 @@
 package via.android.maria.first.easypay.view;
 
+import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
     public void login(View view){
 
     }
