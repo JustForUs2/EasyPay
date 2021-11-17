170b8592114edd735a4d24f50534708c69135c3d
Nina8507 <cntp@outlook.dk>
2021-11-17 16:43:21 +0100

    set up google login

--- app/src/main/java/via/android/maria/first/easypay/viewmodel/LoginViewModel.java
+++ app/src/main/java/via/android/maria/first/easypay/viewmodel/LoginViewModel.java
@@ -3,10 +3,21 @@ package via.android.maria.first.easypay.viewmodel;
 import android.app.Application;
 
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.google.firebase.auth.FirebaseUser;
+
+import via.android.maria.first.easypay.firebasecloud.Authentication.FireUserRepository;
 
 public class LoginViewModel extends AndroidViewModel {
+    private final FireUserRepository fireUserRepository;
 
     public LoginViewModel(Application application) {
         super(application);
+        fireUserRepository = FireUserRepository.getInstance(application);
+    }
+
+    public LiveData<FirebaseUser> getCurrentUser() {
+        return fireUserRepository.getCurrentUser();
     }
 }
