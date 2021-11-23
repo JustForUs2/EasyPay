ffd21fdc63f031f9ab05aa4dd973a195f179a468
MariAsenova <maria@admill.dk>
2021-11-23 13:04:34 +0100

    initial set up for navigation components

--- app/src/main/java/via/android/maria/first/easypay/view/MainActivity.java
+++ app/src/main/java/via/android/maria/first/easypay/view/MainActivity.java
@@ -1,42 +1,132 @@
 package via.android.maria.first.easypay.view;
 
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import via.android.maria.first.easypay.R;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView welcomeBackMessage;
-    private Button dashboard;
     NavController navController;
+    AppBarConfiguration appBarConfiguration;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    BottomNavigationView bottomNavigationView;
+    Toolbar toolbar;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // ####### Navigation Component ###############
-        NavHostFragment navHostFragment =
-                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
-        assert navHostFragment != null;
-        navController = navHostFragment.getNavController();
-
-        dashboard = findViewById(R.id.dashboard);
-        // !! nav_host_fragment - empty container where destinations are swapped in and out
+        initViews();
+        setupNavigation();
     }
 
-    public void signIn(View view) {
-        view.setOnClickListener(view1 -> {
-            navController.navigate(R.id.dashboard);
+    private void initViews() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        toolbar = findViewById(R.id.toolbar);
+    }
+
+    private void setupNavigation() {
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        setSupportActionBar(toolbar);
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations
+        // showing as a burger menu on these screens
+        appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.dashboardFragment,
+                R.id.profileFragment
+        )
+                .setOpenableLayout(drawerLayout)
+                .build();
+
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+        NavigationUI.setupWithNavController(navigationView, navController);
+        setBottomNavigationVisibility();
+    }
+
+    private void setToolBarVisibility() {
+        navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+            if (destination.getId() == R.id.dashboardFragment) {
+                toolbar.setVisibility(View.GONE);
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+            } else {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
+            }
         });
     }
 
+    private void setBottomNavigationVisibility() {
+        navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+            final int id = destination.getId();
+            if (id == R.id.dashboardFragment || id == R.id.profileFragment) {
+                bottomNavigationView.setVisibility(View.VISIBLE);
+            } else {
+                bottomNavigationView.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    private void authorization() {
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+
+        if (currentUser != null) {
+
+        }
+
+    }
+
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START))
+            drawerLayout.closeDrawer(GravityCompat.START);
+        else
+            super.onBackPressed();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.appbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return NavigationUI.onNavDestinationSelected(item, navController) || super.onOptionsItemSelected(item);
+    }
 }
