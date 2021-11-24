5cadd5e62c052ccd1c3df8d06404c64d0308e788
MariAsenova <maria@admill.dk>
2021-11-24 14:43:04 +0100

    repository for transactions

---
+++ app/src/main/java/via/android/maria/first/easypay/viewmodel/TransactionViewModel.java
@@ -0,0 +1,10 @@
+package via.android.maria.first.easypay.viewmodel;
+
+import androidx.lifecycle.ViewModel;
+
+import via.android.maria.first.easypay.repository.TransactionRepository;
+
+// TODO check if there was another view model
+public class TransactionViewModel extends ViewModel {
+    private TransactionRepository transactionRepository;
+}
