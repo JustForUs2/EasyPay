2466e72068510b27c8a8d0658f7706116cda7794
MariAsenova <maria@admill.dk>
2021-11-25 11:06:00 +0100

    adapter for inflating transaction_item

--- app/src/main/java/via/android/maria/first/easypay/view/adapter/TransactionAdapter.java
+++ app/src/main/java/via/android/maria/first/easypay/view/adapter/TransactionAdapter.java
@@ -1,22 +1,38 @@
 package via.android.maria.first.easypay.view.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
+import via.android.maria.first.easypay.R;
+import via.android.maria.first.easypay.model.Transaction;
+
 public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.ViewHolder> {
+    private List<Transaction> transactionList;
+
+    public TransactionAdapter(List<Transaction> transactionList) {
+        this.transactionList = transactionList;
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.transaction_list_item, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+        viewHolder.amount.setText(transactionList.get(position).getAmount());
+        viewHolder.receiverProviderName.setText(transactionList.get(position).getTransferName());
     }
 
     @Override
@@ -24,16 +40,17 @@ public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.
         return 0;
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+    class ViewHolder extends RecyclerView.ViewHolder
     {
+        TextView receiverProviderName;
+        TextView amount;
+        // icon is always the same therefore need to be hardcoded in xml
+        ImageView icon;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-        }
-
-        @Override
-        public void onClick(View view) {
-
+            receiverProviderName = itemView.findViewById(R.id.receiveSender);
+            amount = itemView.findViewById(R.id.amount);
         }
     }
 }
