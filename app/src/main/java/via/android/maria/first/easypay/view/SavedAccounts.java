c96d55b28868b10396f0853d83c70d751d3cef9c
MariAsenova <maria@admill.dk>
2021-11-27 11:32:52 +0100

    switching between list of recipients and add recipient

--- app/src/main/java/via/android/maria/first/easypay/view/SavedAccounts.java
+++ app/src/main/java/via/android/maria/first/easypay/view/SavedAccounts.java
@@ -5,44 +5,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import via.android.maria.first.easypay.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SavedAccounts#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SavedAccounts extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FloatingActionButton fab;
 
     public SavedAccounts() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SavedAccounts.
-     */
-    // TODO: Rename and change types and number of parameters
     public static SavedAccounts newInstance(String param1, String param2) {
         SavedAccounts fragment = new SavedAccounts();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,16 +33,31 @@ public class SavedAccounts extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_saved_accounts, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_saved_accounts, container, false);
+        findViews(view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        fab.setOnClickListener((v) -> {
+            nagivateToAddRecipient();
+        });
+    }
+
+    private void nagivateToAddRecipient() {
+        NavController navController = NavHostFragment.findNavController(this);
+        navController.navigate(R.id.addRecipient);
+    }
+
+    private void findViews(View view) {
+        fab = view.findViewById(R.id.fab_add_recipient);
     }
 }
