a855653a128a47e30b3b4973e41bbc0878217a46
MariAsenova <maria@admill.dk>
2021-11-28 14:05:43 +0100

    add new recipient from recipient fragment

--- app/src/main/java/via/android/maria/first/easypay/view/SelectRecipientFragment.java
+++ app/src/main/java/via/android/maria/first/easypay/view/SelectRecipientFragment.java
@@ -9,18 +9,23 @@ import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.google.android.material.switchmaterial.SwitchMaterial;
 import com.google.android.material.textfield.TextInputEditText;
 
 import via.android.maria.first.easypay.R;
 import via.android.maria.first.easypay.model.Recipient;
+import via.android.maria.first.easypay.viewmodel.RecipientViewModel;
 
 // FRAGMENT B
 public class SelectRecipientFragment extends Fragment {
     private Button addRecipientCTA;
     private TextInputEditText recipientName, recipientAccountNum, recipientSortCode;
+    private SwitchMaterial switchMaterial;
+    private RecipientViewModel recipientViewModel;
 
     public SelectRecipientFragment() {
     }
@@ -41,6 +46,7 @@ public class SelectRecipientFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_select_recipient, container, false);
+        recipientViewModel = new ViewModelProvider(this).get(RecipientViewModel.class);
         findViews(view);
         return view;
     }
@@ -62,6 +68,11 @@ public class SelectRecipientFragment extends Fragment {
                 result.putSerializable("bundleKey", recipient);
                 getParentFragmentManager().setFragmentResult("requestKey", result);
 
+                if (switchMaterial.isChecked())
+                {
+                    recipientViewModel.addRecipient(recipient);
+                }
+
                 navigateToSendTransaction();
             }
         });
@@ -77,5 +88,6 @@ public class SelectRecipientFragment extends Fragment {
         recipientName = view.findViewById(R.id.name);
         recipientAccountNum = view.findViewById(R.id.account_num);
         recipientSortCode = view.findViewById(R.id.sort_code);
+        switchMaterial = view.findViewById(R.id.switch1);
     }
 }
