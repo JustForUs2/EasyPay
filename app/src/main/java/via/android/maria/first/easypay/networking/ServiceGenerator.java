0714b30021c346f836a3ab3c363198846d00b063
MariAsenova <64922954+MariAsenova@users.noreply.github.com>
2021-12-01 15:02:30 +0100

    Delete app/src/main/java/via/android/maria/first/easypay/networking directory

--- app/src/main/java/via/android/maria/first/easypay/networking/ServiceGenerator.java
+++
@@ -1,5 +0,0 @@
-package via.android.maria.first.easypay.networking;
-
-public class ServiceGenerator {
-    private int id;
-}
--- app/src/main/java/via/android/maria/first/easypay/networking/account/api/AccountApi.java
+++
@@ -1,5 +0,0 @@
-package via.android.maria.first.easypay.networking.account.api;
-
-public interface AccountApi {
-
-}
