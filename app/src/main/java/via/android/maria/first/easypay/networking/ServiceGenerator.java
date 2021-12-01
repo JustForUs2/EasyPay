2b8de29be4873f8d69de309d0efca86fe3d76e1c
MariAsenova <64922954+MariAsenova@users.noreply.github.com>
2021-12-01 14:38:08 +0100

    Revert "populating the database through custom API  working"

--- app/src/main/java/via/android/maria/first/easypay/networking/ServiceGenerator.java
+++ app/src/main/java/via/android/maria/first/easypay/networking/ServiceGenerator.java
@@ -1,23 +1,5 @@
 package via.android.maria.first.easypay.networking;
 
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-import via.android.maria.first.easypay.networking.account.api.AccountApi;
-
 public class ServiceGenerator {
-    private static final Retrofit.Builder retrofitBuilder = new Retrofit.Builder()
-            .baseUrl("https://run.mocky.io")
-            .addConverterFactory(GsonConverterFactory.create());
-
-    private static final Retrofit retrofit = retrofitBuilder.build();
-
-    private static final AccountApi accountApi = retrofit.create(AccountApi.class);
-
-    private ServiceGenerator() {
-    }
-
-
-    public static AccountApi getAccountApi() {
-        return accountApi;
-    }
+    private int id;
 }
