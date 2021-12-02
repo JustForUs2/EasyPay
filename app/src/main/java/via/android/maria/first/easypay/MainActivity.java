e570ea4d4ac2f6c601709757c5e03ae8a52a9b1e
MariAsenova <64922954+MariAsenova@users.noreply.github.com>
2021-12-02 12:11:12 +0100

    clean up in main activity and xml

--- app/src/main/java/via/android/maria/first/easypay/MainActivity.java
+++ app/src/main/java/via/android/maria/first/easypay/MainActivity.java
@@ -3,7 +3,6 @@ package via.android.maria.first.easypay;
 import static android.content.ContentValues.TAG;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -11,7 +10,6 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -51,15 +49,6 @@ public class MainActivity extends AppCompatActivity implements PreferenceFragmen
         configureToggleIcon();
         authorization();
         setPreferences();
-    }
-
-    private void setPreferences() {
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
-        if(preferences.getBoolean("darkMode", true))
-            setTheme(R.style.AppTheme);
-        else setTheme(R.style.Theme_EasyPay);
-
-        // setContentView(R.layout.activity_main);
     }
 
     private void initViews() {
@@ -118,8 +107,6 @@ public class MainActivity extends AppCompatActivity implements PreferenceFragmen
             Log.d(TAG, uid);
 
             navController.navigate(R.id.dashboardFragment);
-        } else {
-            Toast.makeText(this, "User not logged in", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -128,6 +115,15 @@ public class MainActivity extends AppCompatActivity implements PreferenceFragmen
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_open, R.string.nav_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+    }
+
+    private void setPreferences() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        if (preferences.getBoolean("darkMode", true))
+            setTheme(R.style.AppTheme);
+        else setTheme(R.style.Theme_EasyPay);
+
+        // setContentView(R.layout.activity_main);
     }
 
     @Override
@@ -151,7 +147,7 @@ public class MainActivity extends AppCompatActivity implements PreferenceFragmen
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == R.id.settings){
+        if (item.getItemId() == R.id.settings) {
             startActivity(new Intent(MainActivity.this, SettingsFragment.class));
         }
         return NavigationUI.onNavDestinationSelected(item, navController) || super.onOptionsItemSelected(item);
